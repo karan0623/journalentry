fa33a710513a4eb3442e919fefa26633d540c824
karan0623 <64561330+karan0623@users.noreply.github.com>
2022-10-11 17:44:07 -0400

    All base changes and .gitingore configuration

---
+++ src/test/java/com/songjournal/journalentry/JournalentryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songjournal.journalentry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalentryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
