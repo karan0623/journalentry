b5a187b8e4f0666c3793af89e5fa6b9df70b96f7
TGentile6 <91705356+TGentile6@users.noreply.github.com>
2022-11-25 10:39:49 -0500

    Added home page

--- src/main/java/com/songjournal/journalentry/JournalController.java
+++ src/main/java/com/songjournal/journalentry/JournalController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/journal")
+@RequestMapping("/")
 public class JournalController {
 	
 	//Mapping for "sign-in"
